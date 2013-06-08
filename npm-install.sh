9ab03d068671a457013977e445ca7745cc1ffe8a
Hu Xiuhan <huxiuhan@gmail.com>
2013-06-08 14:26:12 +0800

    add gitignore

--- npm-install.sh
+++ npm-install.sh
@@ -3,3 +3,7 @@ echo '============   game-server npm installed ============'
 cd ..
 cd ./web-server && npm install -d
 echo '============   web-server npm installed ============'
+cd ..
+cd ./shared && npm install -d
+echo '============   shared npm installed ================'
+
